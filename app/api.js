0f43793c314d1c33e6c727ddccb510b36d9bedb7
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2026-02-27 09:01:26 -0300

    fix: add responses for api offline

--- app/api.js
+++ app/api.js
@@ -7,39 +7,46 @@
 // /alunos/1    Detalhes de um aluno específico pelo ID
 
 
-export async function getCourses(){
-    const endPoint  = `https://lion-school-backend.onrender.com/cursos`;
+export async function getCourses() {
+    const endPoint = `https://lion-school-backend.onrender.com/cursos`;
     const response = await fetch(endPoint);
-    const courses = await response.json();
-    if (response.status == 404)
+    if (response.status == 404) //Caso o retorno esteja vazio
+        response.status;
+    if (!response)  //Caso a API esteja fora do ar
         return false;
+    const courses = await response.json();
     return courses;
 }
 
-export  async function getCourse(courseId){
-    const endPoint  = `https://lion-school-phbo.onrender.com/cursos/${courseId}`;
+export async function getCourse(courseId) {
+    const endPoint = `https://lion-school-phbo.onrender.com/cursos/${courseId}`;
     const response = await fetch(endPoint);
-    if (response.status == 404)
+    if (response.status == 404) //Caso o retorno esteja vazio
+        response.status;
+    if (!response)  //Caso a API esteja fora do ar
         return false;
-    
     const course = await response.json();
-    return  course;
+    return course;
 }
 
 export async function getStudents(courseId) {
-    const endPoint  = `https://lion-school-phbo.onrender.com/alunos?curso_id=${courseId}`;
+    const endPoint = `https://lion-school-phbo.onrender.com/alunos?curso_id=${courseId}`;
     const response = await fetch(endPoint);
-    if (response.status == 404)
+    if (response.status == 404) //Caso o retorno esteja vazio
+        response.status;
+    if (!response)  //Caso a API esteja fora do ar
         return false;
     const students = await response.json();
-    return  students;
+    return students;
 }
 
-export async function getStudent(studentId){
-    const endPoint  = `https://lion-school-phbo.onrender.com/alunos/${studentId}`;
+export async function getStudent(studentId) {
+    const endPoint = `https://lion-school-phbo.onrender.com/alunos/${studentId}`;
     const response = await fetch(endPoint);
-    if (response.status == 404)
+    if (response.status == 404) //Caso o retorno esteja vazio
+        response.status;
+    if (!response)  //Caso a API esteja fora do ar
         return false;
     const student = await response.json();
-    return  student;
+    return student;
 }
